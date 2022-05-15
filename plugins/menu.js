3ca9d01979b80550f23d43e3d6b22e6c699428f8
silana2016 <59898714+silana2016@users.noreply.github.com>
2022-05-15 19:04:39 +0200

    Update menu.js

--- plugins/menu.js
+++ plugins/menu.js
@@ -6,9 +6,9 @@ const os = require('os')
     let context = `
 ╔══════════════════════
 ║
-╠❐ 𝐁𝐎𝐓 𝐍𝐀𝐌𝐄 : 𝐌𝐢𝐥𝐥𝐢𝐞
+╠❐ اسم البوت  : BOBIZ BOT
 ║
-╠❐ 𝐋𝐈𝐁𝐑𝐀𝐑𝐘   : 𝐁𝐚𝐢𝐥𝐞𝐲𝐬-𝐌𝐃
+╠❐ صاحب البوت   :   noureddine ouafy
 ║
 ╠❐ 𝐔𝐏𝐓𝐈𝐌𝐄    : ${uptime}
 ║
@@ -23,7 +23,7 @@ const os = require('os')
 ╠❐ 𝐏𝐋𝐀𝐓𝐅𝐎𝐑𝐌: ${os.type()}
 ║
 ╚════════════════════════`
-await conn.sendTBI2(m.chat, context.trim(), wm,thumburl,`sᴏᴜʀᴄᴇ ᴄᴏᴅᴇ `, `https://github.com/Neeraj-x0/Millie-MD`, null,null, `ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅs`, `${ usedPrefix}lst`,m)
+await conn.sendTBI2(m.chat, context.trim(), wm,thumburl,`انستغرامي `, `instagram.com/noureddine_ouafy`, null,null, `جميع الاوامر`, `${ usedPrefix}lst`,m)
 }
 handler.help = ['ᴍᴇɴᴜ']
 handler.tags = ['main']
